a125b2f8f19fad413452cf9908ba08ee48065d42
Mustafa Sibai <contact@m-sibai.com>
2022-08-11 03:58:41 +0400

    Added Unit1 e-Portfolio Activity

---
+++ Activities/Unit1/app.py
@@ -0,0 +1,29 @@
+#e-Portfolio Activities
+#Develop a Python program and apply protected and unprotected variables within it.
+#In this program, I have created two classes. A parent class called Shape which represents a shape that
+#has both length and width as protected variables. The class also has a function that prints those dimensions.
+#The Shape class has A child class called Rectangle, which inherits from Shape. The Rectangle class calls the 
+#base class (Shape) constructor. The Rectangle class has a function called calculatArea, which prints out
+#the area of the Rectangle.
+
+class Shape:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def printDimensions(self):
+        print("Length:", self._length)
+        print("width:", self._width)
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        Shape.__init__(self, length, width)
+
+    def calculateArea(self):
+        print("Rectangle area:", self._length * self._width)
+
+
+rect = Rectangle(100, 25)
+rect.printDimensions()
+rect.calculateArea()
